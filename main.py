18b934564c39e43a16b7d3013d24970ff4a04f0c
Nobel Arteaga <nobelarteaga@gmail.com>
2026-02-03 18:02:39 -0300

    fetch raw ethereum blocks

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 if not getenv("ETHEREUM_RPC_URL"):
     raise RuntimeError("ETHEREUM_RPC_URL not found in .env")
-if not getenv("POLYGON_RPC_URL"):
-    raise RuntimeError("POLYGON_RPC_URL not found in .env")
 
 def main():
     
     fetch_blocks.fetch_blocks("ethereum")
-    fetch_blocks.fetch_blocks("polygon")
 
 
 if __name__ == "__main__":
